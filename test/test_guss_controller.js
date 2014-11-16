4da98aaf030311149ac995a7ed37b397e155b901
Greg Corradini <gregcorradini@gmail.com>
2014-11-16 04:01:39 +0000

    more fixes for tests

--- test/test_guss_controller.js
+++ test/test_guss_controller.js
@@ -1,14 +1,17 @@
 var test = require( 'tape' ) ,
     jsdom = require( 'jsdom' ) ,
     fs = require( 'fs' ) ,
-    gapi = fs.readFileSync( './gapi-mock.js', 'utf-8' ) ,
-    App = require( './static/js/auth.js' );
+    gapi = require( './gapi-mock.js' ) ,
+    App = fs.readFileSync( './static/js/auth.js', 'utf-8' );
 
 
 jsdom.env( {
     html : "<html><body><a id='build' href='/'>Build a new gus</a></body></html>" ,
     src : [ gapi, App ] ,
     done : function ( errors, window ) {
+
+        var App = window.App;
+        var gapi = window.gapi;
 
         test( 'GussController can be instantiated, state is in proper place, gapi.api_key is set', function ( t ) {
 
